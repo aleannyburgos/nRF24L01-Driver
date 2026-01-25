372d16adcd9f5987b495f6a77629ba2a71b9ea79
Aleanny Burgos <aleannyburgos@gmail.com>
2026-01-25 16:38:50 -0500

    finished first iteration of drivers

--- nRF24L01.c
+++ nRF24L01.c
@@ -1,5 +1,160 @@
-//This will be the code for the driver
-#include <stdio.h>
-#include "nRF24L01.h"
+#include <stdint.h>
+#include "stm32l4xx_hal.h" 
+#include "nrf24l01.h"
 
-//init function
+// GPIO FUNCTIONS
+static inline void CSN_LOW(nrf24_handle_t *d)
+{
+    HAL_GPIO_WritePin(d->csn_port, d->csn_pin, GPIO_PIN_RESET);
+}
+
+static inline void CSN_HIGH(nrf24_handle_t *d)
+{
+    HAL_GPIO_WritePin(d->csn_port, d->csn_pin, GPIO_PIN_SET);
+}
+
+static inline void CE_LOW(nrf24_handle_t *d)
+{
+    HAL_GPIO_WritePin(d->ce_port, d->ce_pin, GPIO_PIN_RESET);
+}
+
+static inline void CE_HIGH(nrf24_handle_t *d)
+{
+    HAL_GPIO_WritePin(d->ce_port, d->ce_pin, GPIO_PIN_SET);
+}
+
+// SPI FUNCTIONS
+static uint8_t nrf24_read_reg(nrf24_handle_t *d, uint8_t reg)
+{
+    uint8_t tx[2] = { CMD_R_REGISTER | reg, CMD_NOP };
+    uint8_t rx[2];
+
+    CSN_LOW(d);
+    HAL_SPI_TransmitReceive(d->hspi, tx, rx, 2, HAL_MAX_DELAY);
+    CSN_HIGH(d);
+
+    return rx[1];
+}
+
+static void nrf24_write_reg(nrf24_handle_t *d, uint8_t reg, uint8_t val)
+{
+    uint8_t tx[2] = { CMD_W_REGISTER | reg, val };
+
+    CSN_LOW(d);
+    HAL_SPI_Transmit(d->hspi, tx, 2, HAL_MAX_DELAY);
+    CSN_HIGH(d);
+}
+
+static void nrf24_write_buf(nrf24_handle_t *d, uint8_t reg,
+                            uint8_t *buf, uint8_t len)
+{
+    uint8_t cmd = CMD_W_REGISTER | reg;
+
+    CSN_LOW(d);
+    HAL_SPI_Transmit(d->hspi, &cmd, 1, HAL_MAX_DELAY);
+    HAL_SPI_Transmit(d->hspi, buf, len, HAL_MAX_DELAY);
+    CSN_HIGH(d);
+}
+
+static void nrf24_read_payload(nrf24_handle_t *d, uint8_t *buf, uint8_t len)
+{
+    uint8_t cmd = CMD_R_RX_PAYLOAD;
+
+    CSN_LOW(d);
+    HAL_SPI_Transmit(d->hspi, &cmd, 1, HAL_MAX_DELAY);
+    HAL_SPI_Receive(d->hspi, buf, len, HAL_MAX_DELAY);
+    CSN_HIGH(d);
+}
+
+static void nrf24_flush_tx(nrf24_handle_t *d)
+{
+    uint8_t cmd = CMD_FLUSH_TX;
+    CSN_LOW(d);
+    HAL_SPI_Transmit(d->hspi, &cmd, 1, HAL_MAX_DELAY);
+    CSN_HIGH(d);
+}
+
+static void nrf24_flush_rx(nrf24_handle_t *d)
+{
+    uint8_t cmd = CMD_FLUSH_RX;
+    CSN_LOW(d);
+    HAL_SPI_Transmit(d->hspi, &cmd, 1, HAL_MAX_DELAY);
+    CSN_HIGH(d);
+}
+
+// PUBLIC FUNCTIONS
+void NRF24_Init(nrf24_handle_t *d)
+{
+    CE_LOW(d);
+    HAL_Delay(5);
+
+    nrf24_write_reg(d, CONFIG_REGISTER,
+                    CONFIG_EN_CRC |
+                    CONFIG_CRCO |
+                    CONFIG_PWR_UP);
+
+    nrf24_write_reg(d, EN_AA_REGISTER, EN_P0);
+    nrf24_write_reg(d, EN_RXADDR_REGISTER, EN_P0);
+    nrf24_write_reg(d, SETUP_AW_REGISTER, SETUP_AW_5B);
+    nrf24_write_reg(d, SETUP_RETR_REGISTER, NRF24_RETR_500US_3);
+    nrf24_write_reg(d, RF_CH_REGISTER, NRF24_CH_40);
+    nrf24_write_reg(d, RF_SETUP_REGISTER, RF_DR_1MBPS | RF_PWR_NEG6DBM);
+    nrf24_write_reg(d, RX_PW_P0_REGISTER, NRF24_DEFAULT_PAYLOAD);
+
+    uint8_t addr[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
+    nrf24_write_buf(d, RX_ADDR_P0_REGISTER, addr, 5);
+    nrf24_write_buf(d, TX_ADDR_REGISTER, addr, 5);
+
+    nrf24_flush_tx(d);
+    nrf24_flush_rx(d);
+
+    CE_LOW(d);
+}
+
+void NRF24_SetTxMode(nrf24_handle_t *d)
+{
+    CE_LOW(d);
+    uint8_t cfg = nrf24_read_reg(d, CONFIG_REGISTER);
+    cfg &= ~CONFIG_PRIM_RX;
+    cfg |= CONFIG_PWR_UP;
+    nrf24_write_reg(d, CONFIG_REGISTER, cfg);
+    HAL_Delay(1);
+}
+
+void NRF24_SetRxMode(nrf24_handle_t *d)
+{
+    CE_LOW(d);
+    uint8_t cfg = nrf24_read_reg(d, CONFIG_REGISTER);
+    cfg |= CONFIG_PRIM_RX | CONFIG_PWR_UP;
+    nrf24_write_reg(d, CONFIG_REGISTER, cfg);
+    nrf24_flush_rx(d);
+    CE_HIGH(d);
+    HAL_Delay(1);
+}
+
+void NRF24_Send(nrf24_handle_t *d, uint8_t *data, uint8_t len)
+{
+    NRF24_SetTxMode(d);
+    nrf24_flush_tx(d);
+
+    uint8_t cmd = CMD_W_TX_PAYLOAD;
+    CSN_LOW(d);
+    HAL_SPI_Transmit(d->hspi, &cmd, 1, HAL_MAX_DELAY);
+    HAL_SPI_Transmit(d->hspi, data, len, HAL_MAX_DELAY);
+    CSN_HIGH(d);
+
+    CE_HIGH(d);
+    HAL_Delay(1);
+    CE_LOW(d);
+}
+
+uint8_t NRF24_DataAvailable(nrf24_handle_t *d)
+{
+    return (nrf24_read_reg(d, STATUS_REGISTER) & CONFIG_MASK_RX_DR);
+}
+
+void NRF24_Read(nrf24_handle_t *d, uint8_t *data, uint8_t len)
+{
+    nrf24_read_payload(d, data, len);
+    nrf24_write_reg(d, STATUS_REGISTER, CONFIG_MASK_RX_DR);
+}
--- nRF24L01.h
+++ nRF24L01.h
@@ -1,8 +1,9 @@
-// This will be the header for the file
 #ifndef NRF24L01_H
-#define NRF24L01_H           
+#define NRF24L01_H
 
-// Register addresses and data
+#include <stdint.h>
+
+// REGISTER MAP
 #define CONFIG_REGISTER                  0x00
 #define EN_AA_REGISTER                   0x01
 #define EN_RXADDR_REGISTER               0x02
@@ -10,8 +11,8 @@
 #define SETUP_RETR_REGISTER              0x04
 #define RF_CH_REGISTER                   0x05
 #define RF_SETUP_REGISTER                0x06
-#define STATUS_REGISTER                  0x07   
-#define OBSERVE_TX_REGISTER              0x08   
+#define STATUS_REGISTER                  0x07
+#define OBSERVE_TX_REGISTER              0x08
 #define CD_REGISTER                      0x09
 #define RX_ADDR_P0_REGISTER              0x0A
 #define RX_ADDR_P1_REGISTER              0x0B
@@ -26,14 +27,14 @@
 #define RX_PW_P3_REGISTER                0x14
 #define RX_PW_P4_REGISTER                0x15
 #define RX_PW_P5_REGISTER                0x16
-#define FIFO_STATUS_REGISTER             0x17   //read
+#define FIFO_STATUS_REGISTER             0x17
 #define DYNPD_REGISTER                   0x1C
 #define FEATURE_REGISTER                 0x1D
 
-// SPI commands
-#define CMD_R_REGISTER                   0x00 
+// SPI COMMANDS
+#define CMD_R_REGISTER                   0x00
 #define CMD_W_REGISTER                   0x20
-#define CMD_R_RX_PAYLOAD                 0x61                
+#define CMD_R_RX_PAYLOAD                 0x61
 #define CMD_W_TX_PAYLOAD                 0xA0
 #define CMD_FLUSH_TX                     0xE1
 #define CMD_FLUSH_RX                     0xE2
@@ -44,71 +45,83 @@
 #define CMD_W_TX_PAYLOAD_NOACK           0xB0
 #define CMD_NOP                          0xFF
 
-// Settings for the registers
-#define CONFIG_MASK_RX_DR (1 << 6)
-#define CONFIG_MASK_TX_DS (1 << 5)
-#define CONFIG_MASK_MAX_RT (1 << 4)
-#define CONFIG_EN_CRC (1 << 3)
-#define CONFIG_CRCO (1 << 2)
-#define CONFIG_PWR_UP (1 << 1)
-#define CONFIG_PRIM_RX (1 << 0)
+// BITWISE FOR CONFIG REGISTER
+#define CONFIG_MASK_RX_DR                (1 << 6)
+#define CONFIG_MASK_TX_DS                (1 << 5)
+#define CONFIG_MASK_MAX_RT               (1 << 4)
+#define CONFIG_EN_CRC                    (1 << 3)
+#define CONFIG_CRCO                      (1 << 2)
+#define CONFIG_PWR_UP                    (1 << 1)
+#define CONFIG_PRIM_RX                   (1 << 0)
 
-// bit masks for the EN_AA Data pipe Register
-#define EN_P0 (1 << 0)
-#define EN_P1 (1 << 1)
-#define EN_P2 (1 << 2)
-#define EN_P3 (1 << 3)
-#define EN_P4 (1 << 4)
-#define EN_P5 (1 << 5)
+// ENABLED DATA PIPES
+#define EN_P0                            (1 << 0)
+#define EN_P1                            (1 << 1)
+#define EN_P2                            (1 << 2)
+#define EN_P3                            (1 << 3)
+#define EN_P4                            (1 << 4)
+#define EN_P5                            (1 << 5)
 
-#define SETUP_AW_3B 0x01
-#define SETUP_AW_4B 0x02
-#define SETUP_AW_5B 0x03
+// ADDRESS WIDTH
+#define SETUP_AW_3B                      0x01
+#define SETUP_AW_4B                      0x02
+#define SETUP_AW_5B                      0x03
 
-// 250us or 500us and 1-3 retransmissions
+// RETRANSMISSION SETTINGS
 typedef enum {
-    SETUP_RETR_DISABLED = 0x00,
-    SETUP_RETR_250us_1T = 0x01,
-    SETUP_RETR_250us_2T = 0x02,
-    SETUP_RETR_250us_3T = 0x03,
-    SETUP_RETR_500us_1T = 0x11,
-    SETUP_RETR_500us_2T = 0x12,  
-    SETUP_RETR_500us_3T = 0x13
-} SETUP_RETR;
+    NRF24_RETR_DISABLED = 0x00,
+    NRF24_RETR_250US_1  = 0x01,
+    NRF24_RETR_250US_2  = 0x02,
+    NRF24_RETR_250US_3  = 0x03,
+    NRF24_RETR_500US_1  = 0x11,
+    NRF24_RETR_500US_2  = 0x12,
+    NRF24_RETR_500US_3  = 0x13
+} nrf24_retr_t;
 
-// working on channels 40, 41 or 42
+// RF CHANNELS
 typedef enum {
-    RF_CH_40 = 0x28,
-    RF_CH_41 = 0x29,
-    RF_CH_42 = 0x2A
-} RF_CH_SETTING;
+    NRF24_CH_40 = 40,
+    NRF24_CH_41 = 41,
+    NRF24_CH_42 = 42
+} nrf24_channel_t;
 
-// bitmasks for RF_SETUP Register
-#define RF_PLL_LOCK   (1 << 4)
-#define RF_DR         (1 << 3) //1 for 2Mbps and 0 for 1Mbps
-#define RF_PWR_1      (1 << 2) //00 for -18dBm, 01 for -12dBm, 10 for -6dBm, 11 for 0dBm
-#define RF_PWR_0      (1 << 1)
-#define LNA_HCURR     (1 << 0)
+// RF SETUP BITS
+#define RF_DR_1MBPS                      0
+#define RF_DR_2MBPS                      (1 << 3)
 
-// Just the first pipe is set to 4B as default
-typedef enum {
-    RX_PW_P0_SIZE = 0x04,
-    RX_PW_P1_SIZE = 0x00,
-    RX_PW_P2_SIZE = 0x00,
-    RX_PW_P3_SIZE = 0x00,
-    RX_PW_P4_SIZE = 0x00,
-    RX_PW_P5_SIZE = 0x00
-} RX_PW_PX_SETTING;
+#define RF_PWR_NEG18DBM                  0
+#define RF_PWR_NEG12DBM                  (1 << 1)
+#define RF_PWR_NEG6DBM                   (1 << 2)
+#define RF_PWR_0DBM                      ((1 << 2) | (1 << 1))
 
-// Bitmasks for the FEATURE register
-#define FEATURE_EN_DYN_ACK (1 << 0)
-#define FEATURE_EN_ACK_PAY (1 << 1)
-#define FEATURE_EN_DPL     (1 << 2)
+// PAYLOAD
+#define NRF24_MAX_PAYLOAD                32
+#define NRF24_DEFAULT_PAYLOAD            4
 
+// DRIVER HANDLE
+typedef struct __SPI_HandleTypeDef SPI_HandleTypeDef;
+typedef struct GPIO_TypeDef GPIO_TypeDef;
 
-// Public Functions
-// nRF24L01_init()
+typedef struct
+{
+    SPI_HandleTypeDef *hspi;
 
-// Private Functions
+    GPIO_TypeDef *ce_port;
+    uint16_t ce_pin;
 
-#endif
+    GPIO_TypeDef *csn_port;
+    uint16_t csn_pin;
+
+} nrf24_handle_t;
+
+// PUBLIC FUNCTIONS
+void NRF24_Init(nrf24_handle_t *dev);
+
+void NRF24_SetTxMode(nrf24_handle_t *dev);
+void NRF24_SetRxMode(nrf24_handle_t *dev);
+
+void NRF24_Send(nrf24_handle_t *dev, uint8_t *data, uint8_t len);
+uint8_t NRF24_DataAvailable(nrf24_handle_t *dev);
+void NRF24_Read(nrf24_handle_t *dev, uint8_t *data, uint8_t len);
+
+#endif /* NRF24L01_H */
